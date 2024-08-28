e640695d3a20441df6eb4bef0a5fafda1f513c9e
JoerenAguila <joerenaguila17@gmail.com>
2024-08-28 11:35:12 +0800

    Add files via upload

---
+++ Exercise Set 02/bitwise2.py
@@ -0,0 +1,3 @@
+n = int(input("Enter an integer n: "))
+result = n & 30
+print(f"{n} AND 30 = {result}")
---
+++ Exercise Set 02/typecasting.py
@@ -0,0 +1,5 @@
+integer_number = int(input("Enter an integer: "))
+float_number = float(input("Enter a float: "))
+converted_integer = float(integer_number)
+difference = converted_integer - float_number
+print(f"Difference: {difference}")
